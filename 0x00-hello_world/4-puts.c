3f34beb02bce796cdd39c6d0c0cd90c1d57ff614
Doro256 <dorothynantumbwe44@gmail.com>
2022-04-07 16:34:31 +0000

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<Stdio.h
+/**
+C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
+Return: Always 0 (SUCCESS)
+**/
+
+int main(void)
+{
+	puts("\"Programming is like building a maultilingual puzzle")
+	return 0;
+}
+
