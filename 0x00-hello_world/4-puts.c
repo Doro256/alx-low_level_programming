67a7e8b82874a0154e40e975286602361446dc83
Doro256 <dorothynantumbwe44@gmail.com>
2022-04-07 16:41:50 +0000

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include<Stdio.h
+#include <Stdio.h>
 /**
-C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line
-Return: Always 0 (SUCCESS)
-**/
+* main - Print "Programming is like building a multilingual puzzle
+* Return: Always 0 (SUCCESS)
+*/
 
 int main(void)
 {
-	puts("\"Programming is like building a maultilingual puzzle")
-	return 0;
+	puts("\"Programming is like building a maultilingual puzzle");
+	return (0);
 }
 
