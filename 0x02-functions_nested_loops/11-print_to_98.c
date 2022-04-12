04a15287f4f3a4ffe6750f95980f661453d45db0
Doro256 <dorothynantumbwe44@gmail.com>
2022-04-12 21:17:31 +0000

    Natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,8 +10,10 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		for (n = n; n < 98; n++)
+		{
 			printf("%d, ", n);
 			printf("%d\n", 98);
+		}
 	}
 	else
 	{
@@ -21,4 +23,5 @@ void print_to_98(int n)
 			printf("%d\n", 98);
 		}
 	}
+}
 
