b2c7a70f7bd5f64b0a2ff01ea0b07def12a06084
Doro256 <dorothynantumbwe44@gmail.com>
2022-04-12 18:19:23 +0000

    prints 10 times the alphabet, in lowercase, followed by a new line.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * print_alphabet_x10() - Print alphabet 10 times
+  * print_alphabet_x10 - Print alphabet 10 times
   * Return : Always 0
   */
 void print_alphabet_x10(void)
