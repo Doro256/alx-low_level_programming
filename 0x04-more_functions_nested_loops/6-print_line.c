9694d64536a4fce238e33fa198ee20f0d29d95e9
Doro256 <dorothynantumbwe44@gmail.com>
2022-04-15 19:12:29 +0000

    draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_line - function that draws a straight line
+  * Return: void
+  */
+
+void print_line(int n);
+
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
+
+
