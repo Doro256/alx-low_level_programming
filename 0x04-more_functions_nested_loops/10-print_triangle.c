107402fa67ef1fb77a24f9148f2764761db60e41
Doro256 <dorothynantumbwe44@gmail.com>
2022-04-15 21:00:41 +0000

    prints a triangle, followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,19 @@
   */
 void print_triangle(int size)
 {
-	int i, size;
+	int i;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 	while (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
 			_putchar((i < size - 1) ? ' ' : '#');
 		}
-		_putchar('\n);
-		size--;
-	}
-	if (size <= 0)
-	{
 		_putchar('\n');
+		size--;
 	}
 }
