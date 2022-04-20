63e9e70b4d1a47db35189f9b26b9aa097f95ce42
Doro256 <dorothynantumbwe44@gmail.com>
2022-04-20 19:31:48 +0000

    Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,11 +7,13 @@
   */
 void print_rev(char *s)
 {
-	int len = 0, i = 0;
+	int i = 0;
 
-	len = _strlen(s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
 
-	for (i = len - 1; i >= 0; i--)
+	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
