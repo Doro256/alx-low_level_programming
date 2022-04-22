e931398c199da7717aec0dcfe35ca0f9bd9b283c
Doro256 <dorothynantumbwe44@gmail.com>
2022-04-22 23:44:37 +0000

    locate

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * _strstr - locates a character in a string
+  * @haystack: is a pointer type char
+  * @needle: is a pointer type char
+  * Return: number of bytes repeated
+  */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
