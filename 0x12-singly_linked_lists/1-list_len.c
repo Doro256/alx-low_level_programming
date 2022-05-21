0789fb912c9cbcf5c4ac72232fb64bc7a1c734f7
Doro256 <dorothynantumbwe44@gmail.com>
2022-05-21 07:47:55 +0000

    Edited Number of elements in list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,10 +8,11 @@ size_t list_len(const list_t *h)
 {
 size_t count = 0;
 
-	while (h -> str != NULL)
+	while (h != NULL)
 	{
-		count++;
-		h = h -> next;
+		if (h -> str != NULL)
+			count++;
+		        h = h -> next;
 	}
 	return(count);
 }
