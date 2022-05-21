0d43829e4e5a2230b8fcd8231932775bb9c0682c
Doro256 <dorothynantumbwe44@gmail.com>
2022-05-21 07:40:43 +0000

    Number of elements in list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "lists.h"
 /**
   * list_len - Finds the number of elements in linked list_t list.
   * @h: pointer to elements in list
@@ -7,7 +7,6 @@
 size_t list_len(const list_t *h)
 {
 	int count = 0;
-	size_t nodes = 0;
 
 	while (h -> str != NULL)
 	{
