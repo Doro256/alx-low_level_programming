72b4e00f39526810fc417ec84bfc2a1924f288c4
Doro256 <dorothynantumbwe44@gmail.com>
2022-05-23 16:11:23 +0000

    return the number of elements in a linked list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+  * listint_len - Print elements of a singly linked list
+  * @h: pointer to list
+  * Return: integer
+  */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *temp;
+	unsigned int counter = 0;
+
+	temp = h;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		counter++;
+		temp = temp->next;
+	}
+	return (counter);
+}
+
