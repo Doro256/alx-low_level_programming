2154f08b9a48df7259f4f2e155397f9aa07c61ca
Doro256 <dorothynantumbwe44@gmail.com>
2022-05-27 16:41:28 +0000

    Binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <math.h>
 /**
   * binary_to_uint - converts a binary number to unsigned int
   * @b: string containing the binary number
@@ -8,21 +7,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int index = 0;
-	unsigned int place_val = 0;
-	int position = 0;
+	int index;
+	unsigned int pVal;
 
 	if (!b)
 		return (0);
 
-
-	while (index >= 0)
+	for (index = 0; b[index]; index++)
 	{
-		place_val = place_val + (b[index] - 48) * pow(2, position);
-		index--;
-		position++;
+		if (b[index] < 48 || b[index] > 49)
+			return (0);
+		pVal = 2 * pVal + (b[index] - 48);
 	}
-	return (place_val);
+	return (pVal);
 }
-
-
