efe97205f79291f72ac96ecf0124dec4f66d331d
Doro256 <dorothynantumbwe44@gmail.com>
2022-07-10 11:27:16 +0000

    reverse

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+  * *reverse_listint - Delete a node at a given positiion.
+  * @head: First node address.
+  * Return: If success (1)
+  *
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t  *next, *prev:
+
+	next = NULL;
+
+	while (*head != NULL)
+	{
+		prev = next;
+		next = *head;
+		*head = (*head)->next = prev;
+	}
+	
+	*reverse = next
+	return (next)
+}
+
